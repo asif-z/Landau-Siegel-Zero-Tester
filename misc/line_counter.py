1a5ebd84470472f307ffffc090a812ae76d33386
kris <zhaohaonan1228@gmail.com>
2025-08-13 23:10:32 -0400

    change some script

--- misc/line_counter.py
+++ misc/line_counter.py
@@ -1,12 +1,14 @@
-# Simple Script that counts number of lines
+# Simple script that counts number of lines
 
 fname = input("Enter the file name: ")
 
-f = open(fname,"r")
+f = open(fname,"r").strip()
 
 counter = 0;
 
 for line in f:
     counter+=1
+
+f.close()
 
 print("Line count: "+ str(counter))
