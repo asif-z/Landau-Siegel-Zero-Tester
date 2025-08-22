482d032eac8308857517102c19f037814299040c
Warheadd <mr.ricklu@hotmail.com>
2025-08-22 12:48:12 -0400

    Added c=0.2 presets

--- misc/main1.c
+++ misc/main1.c
@@ -69,20 +69,39 @@ int init_variables()
 
     //presets:
 
-    //small X (pi(X)~10000)
+    // c = 0.1
+    //small X (pi(X)~10000, X~104729)
     arb_set_str(lambda, "1.4526", prec);
     arb_set_str(phi, "0.22910585", prec);
     arb_set_str(O1, "1.0627878", prec);
 
-    //large X (pi(X)~ 540,000)
+    //large X (pi(X)~ 540,000, X~8003537)
     // arb_set_str(lambda, "1.1122", prec);
     // arb_set_str(phi, "0.23344604", prec);
     // arb_set_str(O1, "1.09531399", prec);
 
-    //extremely large X (pi(X)~ 5million)
+    //extremely large X (pi(X)~ 5million, X~86028121)
     // arb_set_str(lambda, "1", prec);
     // arb_set_str(phi, "0.235032993", prec);
     // arb_set_str(O1, "1.10741261", prec);
+
+
+    // c = 0.2
+    //small X (pi(X)~10000, X~104729)
+    arb_set_str(lambda, "1.87", prec);
+    arb_set_str(phi, "0.224", prec);
+    arb_set_str(O1, "1.02", prec);
+
+    //large X (pi(X)~ 540,000, X~8003537)
+    // arb_set_str(lambda, "1.45", prec);
+    // arb_set_str(phi, "0.229", prec);
+    // arb_set_str(O1, "1.061", prec);
+
+    //extremely large X (pi(X)~ 5million, X~86028121)
+    // arb_set_str(lambda, "1.3", prec);
+    // arb_set_str(phi, "0.231", prec);
+    // arb_set_str(O1, "1.08", prec);
+
 
     // sets sigma, r
     arb_init(sigma);
